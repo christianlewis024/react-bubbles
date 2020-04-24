47ab06c2f03eb876842743731cd893dbc582e02b
Christian Lewis <christianlewis024@gmail.com>
2020-04-24 09:05:13 -0700

    setup the log in form structure

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { axiosWithAuth } from "../components/axiosWithAuth";
 
 const Login = () => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
   return (
     <>
-      <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <h1>Welcome to the Bubble App!</h1>
+        <form>
+          <input type="text" name="username" placeholder="usernam . ." />
+
+          <input type="password" name="password" placeholder="password . ." />
+        </form>
+      </div>
     </>
   );
 };
