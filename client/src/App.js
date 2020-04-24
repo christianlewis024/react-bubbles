fc8b7ddeabdbf9c479b27f86b2a2c8a56632afec
Christian Lewis <christianlewis024@gmail.com>
2020-04-24 08:54:11 -0700

    setup planned  components and app setup

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Login from "./components/Login";
 import "./styles.scss";
+import BubblePage from "./components/BubblePage";
+import PrivateRoute from "./components/PrivateRoute";
 
 function App() {
   return (
